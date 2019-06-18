16282492854d7594e03332cacf180416cbaad136
chen <1193496683@qq.com>
2019-06-18 15:00:42 +0800

    second commit

---
+++ project/project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+profile = os.environ.get('PROJECT_PROFILE','develop')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.%s" % profile)
+
+application = get_wsgi_application()
